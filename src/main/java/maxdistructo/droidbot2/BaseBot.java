26456ad7ed24b16e79327ac92291afd1244065fe
MaxDistructo <micanjvollan@gmail.com>
2017-05-21 12:40:20 -0500

    Use JDA instead of Javacode

--- src/main/java/maxdistructo/droidbot2/BaseBot.java
+++ src/main/java/maxdistructo/droidbot2/BaseBot.java
@@ -1,26 +1,34 @@
 package maxdistructo.droidbot2;
 
-import com.google.common.util.concurrent.FutureCallback;
-import de.btobastian.javacord.DiscordAPI;
-import de.btobastian.javacord.Javacord;
 import de.btobastian.sdcf4j.CommandHandler;
-import de.btobastian.sdcf4j.handler.JavacordHandler;
+import de.btobastian.sdcf4j.handler.JDA3Handler;
 import maxdistructo.droidbot2.commands.Debug;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
 
 public class BaseBot {
     public static String PREFIX;
     public static void main(String[] args){
         String token = "MzE1MzEzOTY3NzU5MDk3ODU3.DANVQw.jygOczrZr40m7OXfBv4b1kYQdPI";
-        DiscordAPI api = Javacord.getApi(token, true);
-        api.connect(new FutureCallback<DiscordAPI>() {
-            public void onSuccess(final DiscordAPI api) {
-                CommandHandler handler = new JavacordHandler(api);
-                handler.registerCommand(new Debug());
-            }
-            public void onFailure(Throwable t) {
-                t.printStackTrace();
-            }
-        });
+        JDA jda;
+        try {
+            jda = new JDABuilder(AccountType.BOT).setToken(token).buildBlocking();
+            CommandHandler handler = new JDA3Handler(jda);
+            handler.registerCommand(new Debug());
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RateLimitedException e) {
+            e.printStackTrace();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
--- src/main/java/maxdistructo/droidbot2/commands/Debug.java
+++ src/main/java/maxdistructo/droidbot2/commands/Debug.java
@@ -1,30 +1,21 @@
 package maxdistructo.droidbot2.commands;
 
-import de.btobastian.javacord.DiscordAPI;
-import de.btobastian.javacord.entities.Channel;
-import de.btobastian.javacord.entities.Server;
-import de.btobastian.javacord.entities.User;
-import de.btobastian.javacord.entities.message.Message;
-import de.btobastian.javacord.entities.permissions.Role;
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
+import net.dv8tion.jda.core.entities.*;
 
 import java.util.Collection;
 
 public class Debug implements CommandExecutor {
     @Command(aliases = {"/debug"}, description = "Shows debug info for making code for the bot.", usage = "/debug")
     public String onDebugCommand(String[] args, Message message) {
-        if(args[1] != null){
-            return "Too Many Arguments. Please just type /debug";
-        }
-        else{
             User author = message.getAuthor();
-            Channel channel = message.getChannelReceiver();
-            Server server = channel.getServer();
-            Collection<Role> roles = author.getRoles(server);
+            MessageChannel channel = message.getChannel();
+            long server = channel.getIdLong();
+            long authorIdLong = author.getIdLong();
 
-            return "You are: " + author + ". \n" + "Your channel is: " + channel + ". \n" + "Your server is: " + server + ". \n";
+            return "You are: " + author + ". \n" + "Your channel is: " + channel + ". \n" + "Your server is: " + server + ". \n" + "Your ID is: " + authorIdLong + ".";
 
-        }
+
         }
 }
