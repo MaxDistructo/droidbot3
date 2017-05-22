9492493f7141c3a85f1000c2d6dea9a5ce3d6326
MaxDistructo <micanjvollan@gmail.com>
2017-05-21 20:59:25 -0500

    Update to a working JSON lib for 1.8 - Sucessfully get Debug to print a bunch of stuff - Start making moderation tools.

--- src/main/java/maxdistructo/droidbot2/commands/moderator.java
+++ src/main/java/maxdistructo/droidbot2/commands/moderator.java
@@ -1,4 +1,14 @@
 package maxdistructo.droidbot2.commands;
 
-public class moderator {
+import de.btobastian.sdcf4j.CommandExecutor;
+import net.dv8tion.jda.core.entities.*;
+
+import java.util.List;
+
+public class moderator implements CommandExecutor {
+    public String onDebugCommand(String[] args, Message message) {
+        User author = message.getAuthor();
+
+        return null;
+    }
 }
