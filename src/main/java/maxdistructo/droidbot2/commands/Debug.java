7c285c0626e63f7e7d3cec97d96e72ad0f995157
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-05-24 18:38:56 -0500

    Update Debug.java

--- src/main/java/maxdistructo/droidbot2/commands/Debug.java
+++ src/main/java/maxdistructo/droidbot2/commands/Debug.java
@@ -2,26 +2,24 @@ package maxdistructo.droidbot2.commands;
 
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
-import net.dv8tion.jda.core.entities.*;
 
-import java.util.Collection;
 import java.util.List;
+
+import sx.blah.discord.handle.impl.obj.Message;
+import sx.blah.discord.handle.obj.*;
 
 public class Debug implements CommandExecutor {
     @Command(aliases = {"/debug"}, description = "Shows debug info for making code for the bot.", usage = "/debug")
     public String onDebugCommand(String[] args, Message message) {
-            User author = message.getAuthor();
-            MessageChannel channel = message.getChannel();
-            long server = channel.getIdLong();
-            long authorIdLong = author.getIdLong();
-            Guild guild = message.getGuild();
-            Member owner = guild.getOwner();
-            User ownerUser = owner.getUser();
-            List<Role> roles = guild.getRoles();
+            IUser author = message.getAuthor();
+            IChannel channel = message.getChannel();
+            IGuild guild = message.getGuild();
+            IUser owner = guild.getOwner();
+            List<IRole> roles = guild.getRoles();
 
 
 
-            return "You are: " + author + ". \n" + "Your channel is: " + channel + ". \n" + "Your server is: " + server + ". \n" + "Your ID is: " + authorIdLong + ". \n" + "Your Owner is: " + owner + ". \n" + "Your server's roles are: " + roles;
+            return "You are: " + author + ". \n" + "Your channel is: " + channel + ". \n" +  "Your Owner is: " + owner + ". \n" + "Your server's roles are: " + roles;
 
 
         }
