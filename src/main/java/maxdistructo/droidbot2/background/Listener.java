e22c82b8e6376c2151295e36dda4f427265fc5a1
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-17 12:23:25 -0600

    Remove Casino Commands

--- src/main/java/maxdistructo/droidbot2/background/Listener.java
+++ src/main/java/maxdistructo/droidbot2/background/Listener.java
@@ -69,18 +69,6 @@ public class Listener {
                 } else if (messageContent[0].equals(prefix + "check")) { //Works
                     Message.sendMessage(message.getChannel(), Message.simpleEmbed(message.getAuthor(), "Check", Check.onCheckCommand(messageContent, message), message));
                     message.delete();
-                } else if (messageContent[0].equals(prefix + "casino") && messageContent[1].equals("info") && mentioned != null && Perms.checkGames(message)) { //Works except for admin commands
-                    message.reply("", Casino.onCasinoInfo(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "casino") && messageContent[1].equals("info") && Perms.checkGames(message)) { //Works except for admin commands
-                    message.reply("", Casino.onCasinoInfo(message));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "casino") && Perms.checkGames(message)) { //Works except for admin commands
-                    message.reply("", Message.simpleEmbed(message.getAuthor(), "Casino", Casino.onCasinoCommand(messageContent, message, message.getAuthor()), message));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "50") || messageContent[0].equals(prefix + "fifty") && Perms.checkGames(message)) { //Works
-                    message.reply("", Message.simpleEmbed(message.getAuthor(), "FiftyFifty", FiftyFifty.onFiftyCommand(messageContent, message), message));
-                    message.delete();
                 } else if (messageContent[0].equals(prefix + "fortune")) { //Works
                     message.reply("", Message.simpleEmbed(message.getAuthor(), "Fortune", Fortune.onFortuneCommand(messageContent, message), message));
                     message.delete();
@@ -103,9 +91,6 @@ public class Listener {
                     else{
                         Message.sendDM(message.getAuthor(), Help.onHelpCommand());
                     }
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "allin") && Perms.checkGames(message)) {
-                    message.reply("", Message.simpleEmbed(message.getAuthor(), "Allin", Allin.onAllinCommand(messageContent, message), message));
                     message.delete();
                 } else if (messageContent[0].equals(prefix + "say") && channelMention != null) {
                     Message.sendMessage(channelMention, Say.onSayCommand(messageContent, message, channelMention));
