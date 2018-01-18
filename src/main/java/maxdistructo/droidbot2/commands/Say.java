fee2cbb05974f34965e992557b73cb0fbf4c74e0
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-18 08:21:09 -0600

    Add image support to say command.

--- src/main/java/maxdistructo/droidbot2/commands/Say.java
+++ src/main/java/maxdistructo/droidbot2/commands/Say.java
@@ -11,16 +11,28 @@ import maxdistructo.droidbot2.core.Utils;
 public class Say {
     public static String onSayCommand(Object[] args, IMessage message, IChannel mentionedChannel) throws RateLimitException, DiscordException, MissingPermissionsException {
         boolean anotherChannel = false;
+        List<IMessage.Attachment> attachments = null;
+        attachments = message.getAttachments();
         if(mentionedChannel != null){
             anotherChannel = true;
         }
         
         if (anotherChannel) {
-            return Utils.makeNewString(args, 2);
+            if(attachments != null){
+            return Utils.makeNewString(args, 2) + attachements.get(0).getUrl();
+            }
+            else{
+                return Utils.makeNewString(args, 2);
+            }
         }
 
         else{
-            return Utils.makeNewString(args, 1);
+            if(attachments != null){
+                return Utils.makeNewString(args, 1) + attachments.get(0).getUrl();
+            }
+            else{ //This is technically not required though I want to make sure it does what it is suppost to do.
+               return Utils.makeNewString(args, 1); 
+            }
         }
     }
 }
