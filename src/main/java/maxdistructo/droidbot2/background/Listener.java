04ecdd70515c2710f0af50f381f6c2b9cb02cc2b
MaxDistructo <micanjvollan@gmail.com>
2018-01-28 19:32:35 -0600

    Fix Git again

--- src/main/java/maxdistructo/droidbot2/background/Listener.java
+++ src/main/java/maxdistructo/droidbot2/background/Listener.java
@@ -5,16 +5,13 @@ import maxdistructo.droidbot2.BaseBot;
 import maxdistructo.droidbot2.background.filter.SwearFilter;
 import maxdistructo.droidbot2.core.message.Message;
 import maxdistructo.droidbot2.commands.*;
-import maxdistructo.droidbot2.commands.casino.Casino;
-import maxdistructo.droidbot2.commands.casino.CasinoConfig;
 import maxdistructo.droidbot2.core.Config;
 import maxdistructo.droidbot2.core.Perms;
 import maxdistructo.droidbot2.core.Roles;
 import maxdistructo.droidbot2.core.Utils;
 import maxdistructo.droidbot2.core.Client;
-import org.json.JSONException;
+import org.apache.commons.io.FileUtils;
 import sx.blah.discord.api.events.EventSubscriber;
-import sx.blah.discord.handle.impl.events.guild.GuildCreateEvent;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.impl.events.guild.voice.user.UserVoiceChannelJoinEvent;
 import sx.blah.discord.handle.impl.events.guild.voice.user.UserVoiceChannelLeaveEvent;
@@ -26,26 +23,16 @@ import sx.blah.discord.util.MissingPermissionsException;
 import sx.blah.discord.util.RateLimitException;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.EnumSet;
+import java.net.URL;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import sx.blah.discord.util.RoleBuilder;
 
 import static maxdistructo.droidbot2.BaseBot.client;
 import static maxdistructo.droidbot2.core.Utils.s;
 
 public class Listener {
-    public static boolean blackJackRunning = false;
-    public static String blackjackAnswer;
     public static String prefix = "!"; //Change back to ! for release!
-    public static String triviaAnswer;
-    public static boolean triviaRunning = true;
 
     @EventSubscriber
     public void onMessageReceivedEvent(MessageReceivedEvent event) throws RateLimitException, DiscordException, MissingPermissionsException {
@@ -53,16 +40,16 @@ public class Listener {
             IMessage message = event.getMessage();
             IGuild guild = message.getGuild();
             prefix = Client.prefix; // To allow for easy compatability with old code. All new code will reference #Client.prefix directly.
-            
+
             IChannel channelMention = Utils.getMentionedChannel(message);
             IUser mentioned = Utils.getMentionedUser(message);
-            
+
             String content = message.getContent();
             Object messageContent[] = content.split(" ");
             SwearFilter.filter(message, messageContent);
 
             if (!Roles.checkForBotAbuse(message)) {
-                  if (messageContent[0].equals(prefix + "check")) { //Works
+                if (messageContent[0].equals(prefix + "check")) { //Works
                     Message.sendMessage(message.getChannel(), Message.simpleEmbed(message.getAuthor(), "Check", Check.onCheckCommand(messageContent, message), message));
                     message.delete();
                 } else if (messageContent[0].equals(prefix + "info")) { //Works Well
@@ -114,34 +101,32 @@ public class Listener {
                 } else if (messageContent[0].equals(prefix + "emote")) {
                     Emote.onEmoteCommand(message, messageContent);
                     message.delete();
-                } else if (messageContent[0].equals(prefix + "admin") && messageContent[1].equals("fixPerms") && !messageContent[2].equals(null)){
-                    if(Perms.checkOwner(message)){
+                } else if (messageContent[0].equals(prefix + "admin") && messageContent[1].equals("fixPerms") && !messageContent[2].equals(null)) {
+                    if (Perms.checkOwner(message)) {
                         Message.sendDM(message.getGuild().getOwner(), "CasinoBot has left your server because the bot owner though it was missing perms or its permissions were screwed up. Please use this url to re-add CasinoBot to your server. Your server's data has not been affected. https://discordapp.com/oauth2/authorize?client_id=315313967759097857&scope=bot&permissions=470281296");
                         message.getGuild().leave();
                     }
-                } else if (messageContent[0].equals(prefix + "@admin") && messageContent[1].equals("setColor") && !messageContent[2].equals(null) && Perms.checkMod(message)){
-                    Roles.changeColor(Roles.getRole(message,(String) messageContent[2]), (String) messageContent[3]);
+                } else if (messageContent[0].equals(prefix + "@admin") && messageContent[1].equals("setColor") && !messageContent[2].equals(null) && Perms.checkMod(message)) {
+                    Roles.changeColor(Roles.getRole(message, (String) messageContent[2]), (String) messageContent[3]);
                     message.delete();
-                } else if (messageContent[0].equals(prefix + "@mute") && !messageContent[2].equals(null) && Perms.checkAdmin(message)){ //!@mute @User time
+                } else if (messageContent[0].equals(prefix + "@mute") && !messageContent[2].equals(null) && Perms.checkAdmin(message)) { //!@mute @User time
                     Admin.muteUser(message, mentioned, Config.converToInt(messageContent[2]));
                     message.delete();
-                } else if (messageContent[0].equals(prefix + "@unmute") && Perms.checkAdmin(message) && channelMention != null){
+                } else if (messageContent[0].equals(prefix + "@unmute") && Perms.checkAdmin(message) && channelMention != null) {
                     Admin.unmuteUser(message, mentioned, channelMention);
                     message.delete();
-                } else if (messageContent[0].equals(prefix + "@unmute") && Perms.checkAdmin(message)){
+                } else if (messageContent[0].equals(prefix + "@unmute") && Perms.checkAdmin(message)) {
                     Admin.unmuteUser(message, mentioned);
                     message.delete();
-                } else if (messageContent[0].equals(prefix + "@announce")){
+                } else if (messageContent[0].equals(prefix + "@announce")) {
                     Admin.onAnnounceCommand(messageContent, message);
                     message.delete();
-                } else if (messageContent[0].equals(prefix + "fixServer") && Perms.checkOwner_Guild(message)){ //Due to requirement of server configs (Blame Swear Filter), this command is separated so that if errors are being thrown this command can still run.
-                    IGuild guild = message.getGuild();
-                try{
-                    FileUtils.copyURLToFile(new URL("https://maxdistructo.github.io/droidbot2/downloads/config/defaultconfig.txt"), new File(s + "droidbot/config/" + message.getGuild().getID() + ".txt"));
-                }
-                catch(Exception e){
-                    Message.throwError(e, message);
-                }
+                } else if (messageContent[0].equals(prefix + "fixServer") && Perms.checkOwner_Guild(message)) { //Due to requirement of server configs (Blame Swear Filter), this command is separated so that if errors are being thrown this command can still run.
+                    try {
+                        FileUtils.copyURLToFile(new URL("https://maxdistructo.github.io/droidbot2/downloads/config/defaultconfig.txt"), new File(s + "droidbot/config/" + message.getGuild().getLongID() + ".txt"));
+                    } catch (Exception e) {
+                        Message.throwError(e, message);
+                    }
                     Roles.makeNewRole(guild, "Voice Chatting", false, false);
                     Roles.makeNewRole(guild, "Payday", false, false);
                     Roles.makeNewRole(guild, "Bot Abuser", false, false);
@@ -149,7 +134,7 @@ public class Listener {
                     Message.sendMessage(guild.getDefaultChannel(), "Thank you for letting me join your server. I am " + client.getOurUser().getName() + " and my features can be found by using the command " + prefix + "help.");
 
                 }
-                
+
             } else if (content.charAt(0) == prefix.charAt(0)) {
                 message.reply("Please wait until you have lost your Bot Abuser role to use this command.");
             }
@@ -195,11 +180,11 @@ public class Listener {
             } else {
                 name = client.getApplicationName();
             }
-            CasinoConfig.doPaydayReset(guild);
             i++;
         }
 
     }
+}
     
     /*public static void onGuildJoinEvent(GuildCreateEvent event){
         IGuild guild = event.getGuild();
