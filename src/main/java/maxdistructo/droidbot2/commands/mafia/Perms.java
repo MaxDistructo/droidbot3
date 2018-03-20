637c2e88b29466e3fa0181a431090602731e885f
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-03-20 09:09:35 -0500

    Create Perms.java

---
+++ src/main/java/maxdistructo/droidbot2/commands/mafia/Perms.java
@@ -0,0 +1,19 @@
+package maxdistructo.droidbot2.commands.mafia
+
+public class Perms{
+
+public static boolean checkMod(){
+
+}
+public static boolean checkAdmin(){
+
+}
+
+public static boolean checkOwner_Guild(){
+
+}
+public static boolean checkOwner(){
+
+}
+
+}
