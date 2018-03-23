42ac1edc962fcaaaedaa3d2c9426174cebec8395
MaxDistructo <micanjvollan@gmail.com>
2018-03-22 22:02:38 -0500

    Mafia Listener Commit

--- src/main/java/maxdistructo/droidbot2/BaseBot.kt
+++ src/main/java/maxdistructo/droidbot2/BaseBot.kt
@@ -4,14 +4,12 @@ import maxdistructo.droidbot2.background.*
 import maxdistructo.droidbot2.commands.mafia.MafiaListener
 import maxdistructo.droidbot2.core.Config
 import maxdistructo.droidbot2.core.Client
-import org.slf4j.Logger
 import sx.blah.discord.api.IDiscordClient
 
 object BaseBot {
     lateinit var client: IDiscordClient
     var LOGGER = Client.LOGGER
-    val listener = Listener()
-
+    var listener = Listener()
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -22,7 +20,7 @@ object BaseBot {
         //client.getDispatcher().registerListener(new AudioMain());
         //LOGGER.info("Registered Audio Commands ");
         //Help.registerHelp();
-        client.dispatcher.registerListener(listener)
+        client.dispatcher.registerListener(Listener())
         LOGGER.info("Registered Listener")
         client.dispatcher.registerListener(MafiaListener())
         LOGGER.info("Registered Mafia Listener. PLAY ON!")
